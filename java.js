b744c944810866d6070c2c59d10a05c62819123f
Thais Santos <thais.santos832@etec.sp.gov.br>
2023-10-10 20:40:51 -0300

    uppando

---
+++ java.js
@@ -0,0 +1,12 @@
+"use strict";
+//alert("test"); //Mensagaem para o usuário
+/*
+comentario
+comentário
+*/
+//declarando variável
+let nome;
+//atribuindo valor a uma variável
+nome="Josney";
+//exibindo no console
+console.log(nome);
